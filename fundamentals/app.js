bf5be5547eda5cab8e986a7f2944209c6a93c972
jhonatanmizu <natanjesuss20@gmail.com>
2022-07-21 23:26:20 -0300

    Finished basics

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -5,13 +5,26 @@ Vue.createApp({
             name:'Jhonatan'
         }
     },
+    computed: {
+        fullName(){
+            return `${this.name}  Oliveira`
+        }
+    },
+    watch: {
+        name(){
+            alert("CHANGED")
+        }
+    },
     methods: {
         sayHello(){
-            alert("HELLO")
+            // alert("HELLO")
+        },
+        log(){
+            console.log("LOGANDO");
         }
     },
     mounted() {
         this.sayHello()
     },
-    
+
 }).mount('#app')
