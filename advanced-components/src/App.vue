58480a47f9dc21d14a5bda0ea334d00334052da4
jhonatanmizu <natanjesuss20@gmail.com>
2022-07-24 15:15:20 -0300

    used events emmiter

--- advanced-components/src/App.vue
+++ advanced-components/src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {{ name }}
+  <Header :name="name" @so="so" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Header from './components/Header.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      name: "APP"
+    }
+  },
+  methods: {
+    so() {
+      console.log("SOOSJS");
+      this.name = this.name + "1"
+
+    }
+  },
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+$color: #333;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $color;
   margin-top: 60px;
 }
 </style>
---
+++ advanced-components/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <button @click="doSomenthing">alog</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  created() {
+    this.message()
+  },
+  data() {
+    return {};
+  },
+  props: {
+    name: String
+  },
+  methods: {
+    message() {
+      // alert("MENSAGEM QUALQUER")
+    },
+    doSomenthing() {
+      this.$emit("so")
+      console.log("JKLGJFKLGJG");
+      this.name + 1
+    }
+  },
+};
+</script>
+
+<style  scoped>
+</style>
